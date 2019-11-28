a6236bd504825398b7dcfe97449a4ef2cd5c14e3
Thiago <thiago.comelli@outlook.com>
2019-11-28 18:46:06 -0300

    asdf

---
+++ database.py
@@ -0,0 +1,13 @@
+import pymongo
+from pymongo import MongoClient
+
+cluester = MongoClient("mongodb+srv://thiago:1234@cluster0-jhejj.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = cluester['discord']
+collection = db['discord-botimage']
+
+
+def update(id):
+    if collection.find_one({'_id':id}):
+        collection.update_one({'_id': id}, {'$inc': {'value': 1}})
+    else:
+        collection.insert_one({'_id':id,'value':1})
--- misc/main.py
+++ misc/main.py
@@ -2,6 +2,14 @@ import discord
 import asyncio
 from PIL import Image, ImageDraw, ImageFont
 from discord.ext import commands
+import database
+from database import *
+from random import randint
+
+cluester = MongoClient("mongodb+srv://thiago:1234@cluster0-jhejj.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = cluester['test']
+collection = db
+
 
 client = commands.Bot(command_prefix=".")
 
@@ -10,48 +18,15 @@ async def on_ready():
     print("Ready!")
 
 
-# @client.event
-# async def on_member_join(member):
-#     with open("dados.json", "r") as f:
-#         users = json.load(f)
-#
-#     await update_data(users,member)
-#
-#     with open("dados.json", "w") as f:
-#         json.dump(users,f)
-#
-#
-#
-# async def update_data(users,user,qtde):
-#     if not user.id in users:
-#         users[user.id] = {}
-#         users[user.id]['messages'] = 1
-#
-#     else:
-#         print("oi")
-#         users[user.id]['messages'] += qtde
-
-
 @client.event
 async def on_message(message):
     id = client.get_guild(145551479292428288)
     channels = ["bot"]
-    valid_users = ["THIAGO#6785"]
+    # valid_users = ["THIAGO#6785"]
 
+    update(str(message.author))
 
-
-
-    # with open("dados.json", "r") as f:
-    #     users = json.load(f)
-    #
-    # await update_data(users,message.author,1)
-    #
-    # with open("dados.json", "w") as f:
-    #     json.dump(users,f)
-
-
-
-    if str(message.channel) in channels and str(message.author) in valid_users:
+    if str(message.channel) in channels:
         if message.content == "!hello":
             await message.channel.send("Hi", file=discord.File('images.jpg'))
 
@@ -71,15 +46,12 @@ async def on_message(message):
             else:
                 palavra = str(palavras[4:])
                 img = Image.new('RGB', (int(palavras[1]), int(palavras[2])), color=(255, 255, 255))
-                fnt = ImageFont.truetype('times.ttf', int(palavras[3]))
+                fnt = ImageFont.truetype('misc/times.ttf', int(palavras[3]))
                 d = ImageDraw.Draw(img)
                 d.text((5, 0), "{}".format(criaFrase(palavras[4:])), font=fnt, fill=(0, 0, 0))
-                img.save('{}.png'.format(str(message.author)))
-                await message.channel.send(file=discord.File("{}.png".format(str(message.author))))
+                img.save('images/{}.png'.format(str(message.author)))
+                await message.channel.send(file=discord.File("images/{}.png".format(str(message.author))))
 
-
-        else:
-            print("{} tried to do command {}, in channel {}".format(message.author, message.content, message.channel))
 
 def criaFrase(lista):
     frase = ""
